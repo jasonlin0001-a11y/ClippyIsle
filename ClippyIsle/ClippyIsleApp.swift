b873c34b23c95ec2085df7b4e472ad185797a7c0
SHIH CHIEH LIN <jye90210@gmail.com>
2025-10-25 23:08:57 +0800

    Initial Commit

---
+++ ClippyIsle/ClippyIsleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClippyIsleApp.swift
+//  ClippyIsle
+//
+//  Created by Jason Lin on 2025/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ClippyIsleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
